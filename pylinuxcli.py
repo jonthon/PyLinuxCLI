eae1360f9e4e024cbfa31a853cac9c634c4b9f13
Johnson Ngao <developerjon@outlook.com>
2023-06-09 13:09:50 -0400

    Add files via upload

--- pylinuxcli.py
+++ pylinuxcli.py
@@ -2,8 +2,73 @@
 import os, sys, pty, time, socket
 from socketserver import (BaseRequestHandler, ThreadingTCPServer)
 
-WAITTIME = 0.2       # secs
+WAITTIME = 0.5       # secs
 MAXBYTES = 1024 ** 1 # 1 KB
+
+class Done(Exception): pass
+
+class TerminalIO:
+    def __init__(self, io, waittime=WAITTIME, maxbytes=MAXBYTES):
+        self.io = io
+        self.waittime = waittime
+        self.maxbytes = maxbytes
+    def read(self):
+        time.sleep(self.waittime)
+        return os.read(self.io, self.maxbytes)
+    def write(self, data):
+        return os.write(self.io, data)
+    def block(self):
+        return os.set_blocking(self.io, True)
+    def unblock(self):
+        return os.set_blocking(self.io, False)
+    def close(self): os.close(self.io)
+
+class SocketIO:
+    def __init__(self, conn, waittime=WAITTIME, maxbytes=MAXBYTES):
+        self.conn = conn
+        self.waittime = waittime
+        self.maxbytes = maxbytes
+    def read(self):
+        time.sleep(self.waittime)
+        return self.conn.recv(self.maxbytes)
+    def write(self, data):
+        return self.conn.send(data)
+    def block(self):
+        return self.conn.setblocking(True)
+    def unblock(self):
+        return self.conn.setblocking(False)
+    def close(self):
+        self.conn.close()
+
+def communicate(terminalIO, input, output):
+    while True:
+        try:
+            terminalIO.unblock()            # unblock cli
+            while True: output(terminalIO)  # read all output
+        except BlockingIOError: 
+            terminalIO.block()              # re-block cli
+        try:
+            while True: input(terminalIO)   # write to cli ...
+        except Done: pass                   # till done
+
+def input(handler):
+    done = False            # remember state
+    def input(terminalIO):
+        nonlocal done
+        if done: done = False; raise Done
+        else:    handler(terminalIO); done = True
+    return input
+
+@input
+def localinput(terminalIO): 
+    data = sys.stdin.readline().encode()
+    terminalIO.write(data)
+
+def localoutput(terminalIO):
+    data = terminalIO.read()
+    data = data.decode()
+    sys.stdout.write(data)
+    sys.stdout.flush()
 
 # cmd line prog. Could be customized 
 # to use diff other than os.execvp
@@ -11,72 +76,15 @@ def runprogram(cmd):
     "Program/Command to be run on CLI"
     args = cmd.strip().split()
     name = args[0]
-    os.execvp(name, args)  
-
-def read( i, maxbytes): return os.read(i, maxbytes)
-def write(o, data):     return os.write(o, data)
-def block(io):   return os.set_blocking(io, True)
-def unblock(io): return os.set_blocking(io, False)
-
-def communicate(i1, o1, i2, o2, input, output, *, waittime=WAITTIME, 
-                                                  maxbytes=MAXBYTES):
-    
-    class Participant:
-        def __init__(self, i, o):
-            self.i = i
-            self.o = o
-
-    sender   = Participant(i1, o1)  
-    receiver = Participant(i2, o2)
-    while True:
-        try:
-            unblock(sender.i)                               # free sender. 
-            while True:
-                if sender.i == i2: input(sender.o)          # write first   ..
-                time.sleep(waittime)                        # wait a little ..
-                write(receiver.o, read(sender.i, maxbytes)) # then read.
-        except BlockingIOError:
-            if receiver.o == o2:                            # read prog out.
-                try:
-                    unblock(receiver.i)
-                    time.sleep(waittime)
-                    while True: output(receiver.i)
-                except BlockingIOError: pass
-                finally: block(receiver.i)         
-            temp     = sender
-            sender   = receiver                             # switch roles.
-            receiver = temp
-        finally: block(sender.i)                            # reblock sender. 
-
-def input(handler):
-    done = False    # remember state
-    def input(fd):
-        nonlocal done
-        if done: 
-            done = False
-            raise BlockingIOError
-        handler(fd)
-        done = True
-    return input
-
-@input
-def localinput(fd): 
-    data = sys.stdin.readline().encode()
-    os.write(fd, data)
-
-def localoutput(fd):
-    data = os.read(fd, MAXBYTES)
-    data = data.decode()
-    sys.stdout.write(data)
-    sys.stdout.flush()
+    os.execvp(name, args)
 
 # linux terminal
 def startterminal(cmd, input, output):
-    i2, o2 = os.pipe()
     progID, progIO = pty.fork()
     if not progID: runprogram(cmd)
-    communicate(progIO, progIO, i2, o2, input, output)
+    communicate(TerminalIO(progIO), input, output)
 
+# run locally
 def startlocalterminal(cmd):
     startterminal(cmd, localinput, localoutput)
 
@@ -84,31 +92,31 @@ class SocketTerminalHandler(BaseRequestHandler):
     "Server handler"
     def handle(self):
         @input
-        def socketinput(fd):
+        def socketinput(terminalIO):
             data = self.request.recv(MAXBYTES)    # 1KB at a time 
-            os.write(fd, data)
-        def socketoutput(fd):
-            data = os.read(fd, MAXBYTES)
+            terminalIO.write(data)
+        def socketoutput(terminalIO):
+            data = terminalIO.read()
             self.request.send(data)
-        startterminal(self.request.recv(MAXBYTES), socketinput, 
-                                                   socketoutput)
-# run this on server device
-class StartSocketTerminal:
-    "Server interface"
+        cmd = self.request.recv(MAXBYTES)
+        startterminal(cmd, socketinput, socketoutput)
+
+# run this on server machine
+class StartTerminalServer:
     def __init__(self, name, port):
-         ThreadingTCPServer((name, port), 
-                            SocketTerminalHandler).serve_forever()
+         IP      = name, port
+         Handler = SocketTerminalHandler
+         Server  = ThreadingTCPServer
+         Server(IP, Handler).serve_forever()
 
 # run this on a client device
-class StartUserSocketTerminal:
-    "Client interface"
-    def __init__(self, name, port):
+class StartSocketTerminal:
+    def __init__(self, name, port, cmd=None):
         conn = socket.socket()
+        cmd  = 'bash' if not cmd else cmd
+        cmd  = (cmd + '\n').encode()
         conn.connect((name, port))
-        conn.send(b'bash\n')
-        while True:
-            odata = conn.recv(1024 ** 2)
-            print(odata.decode(), end='')
-            idata = sys.stdin.readline()
-            conn.send(idata.encode())
-            
+        conn.send(cmd)
+        terminal = SocketIO(conn, 1)
+        communicate(terminal, localinput, localoutput)
+        
